c4ff0716f5edeaa092a4b58020234dc1d4408d1e
GayBoiJarleru <jarl.bergstrom@elev.ga.ntig.se>
2019-11-07 11:11:03 +0100

    Jarl Alexander jachob

---
+++ src/Stenisaxiposeru.java
@@ -0,0 +1,32 @@
+//Skapad av Jarleru Alexanderu och jukkasjärvi
+// 2019 - 11 - 07
+import java.util.*;
+import javax.swing.*;
+public class Stenisaxiposeru {
+
+    public static void main(String[] args) {
+        int a = 0;
+        a = Integer.parseInt(JOptionPane.showInputDialog(null, "1 eller 2"));
+        int 1 = 1;
+        int 1 = 2;
+
+        int a = 0;
+        int saxi = 0;
+        int steni = 1;
+        int poseru = 2;
+
+        if 1 = 1{
+
+        }
+        if 1 = 2{
+
+        }
+
+      
+            }
+        }
+
+
+
+    }
+}
